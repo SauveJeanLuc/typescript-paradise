331b653dc07236ede7ec8700e3b337241973a703
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-08-03 22:37:39 +0200

    basic variable types

--- main.js
+++ main.js
@@ -6,3 +6,6 @@ var x = 10; // Can be done without initialisation
 var y = 20; // Always initialized with a value, can never be re-assigned
 var sum;
 var title = 'Sympa';
+var isBeginner = true;
+var total = 0;
+var name = 'Vishwas';
--- main.ts
+++ main.ts
@@ -7,3 +7,7 @@ const y = 20; // Always initialized with a value, can never be re-assigned
 
 let sum;
 const title = 'Sympa';
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'Vishwas';

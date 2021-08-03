2833f68ef199285abf91f192c1514f92cc870cd7
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-08-03 22:33:51 +0200

    let and const

--- main.js
+++ main.js
@@ -2,3 +2,6 @@
 exports.__esModule = true;
 var message = "Welcome back";
 console.log(message);
+var x = 10; // Can be done without initialisation
+var y = 20; // Always initialized with a value, can never be re-assigned
+var x = 30;
--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
 export {}
 let message = "Welcome back";
 console.log(message);
+
+let x =10; // Can be done without initialisation
+const y = 20; // Always initialized with a value, can never be re-assigned
+
+let x = 30;
